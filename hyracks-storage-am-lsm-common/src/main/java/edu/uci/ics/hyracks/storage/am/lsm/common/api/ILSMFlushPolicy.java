209094e3b3fa1ffc830ef11b9ee1ecb3379a6fd5
zheilbron <zheilbron@123451ca-8445-de46-9d55-352943316053>
2012-06-17 01:03:58 +0000

    forgot files in last commit :(
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_lsm_tree@1566 123451ca-8445-de46-9d55-352943316053

--- hyracks-storage-am-lsm-common/src/main/java/edu/uci/ics/hyracks/storage/am/lsm/common/api/ILSMFlushPolicy.java
+++ hyracks-storage-am-lsm-common/src/main/java/edu/uci/ics/hyracks/storage/am/lsm/common/api/ILSMFlushPolicy.java
@@ -2,5 +2,5 @@ package edu.uci.ics.hyracks.storage.am.lsm.common.api;
 
 
 public interface ILSMFlushPolicy {
-    public void shouldFlush(ILSMIndex harness);
+    public void shouldFlush(ILSMIndex index);
 }
---
+++ hyracks-storage-am-lsm-common/src/main/java/edu/uci/ics/hyracks/storage/am/lsm/common/impls/SequentialFlushPolicy.java
@@ -0,0 +1,41 @@
+package edu.uci.ics.hyracks.storage.am.lsm.common.impls;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
+import edu.uci.ics.hyracks.api.exceptions.HyracksDataException;
+import edu.uci.ics.hyracks.storage.am.common.api.IndexException;
+import edu.uci.ics.hyracks.storage.am.lsm.common.api.ILSMFlushPolicy;
+import edu.uci.ics.hyracks.storage.am.lsm.common.api.ILSMIndex;
+import edu.uci.ics.hyracks.storage.am.lsm.common.api.ILSMIndexAccessor;
+
+public enum SequentialFlushPolicy implements ILSMFlushPolicy {
+    INSTANCE;
+
+    private static final Logger LOGGER = Logger.getLogger(SequentialFlushPolicy.class.getName());
+
+    private final ExecutorService executor;
+
+    private SequentialFlushPolicy() {
+        executor = Executors.newSingleThreadExecutor();
+    }
+
+    @Override
+    public void shouldFlush(final ILSMIndex index) {
+        executor.submit(new Runnable() {
+            public void run() {
+                try {
+                    LOGGER.info("flushing");
+                    ((ILSMIndexAccessor) index.createAccessor()).flush();
+                    LOGGER.info("finished flushing");
+                } catch (HyracksDataException e) {
+                    LOGGER.info(e.getMessage());
+                } catch (IndexException e) {
+                    LOGGER.info(e.getMessage());
+                }
+                LOGGER.info("Thread should end");
+            }
+        });
+    }
+}
