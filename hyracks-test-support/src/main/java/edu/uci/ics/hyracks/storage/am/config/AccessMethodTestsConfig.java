18c13d5f595321468971fb73eaf45324dab27504
alexander.behm <alexander.behm@123451ca-8445-de46-9d55-352943316053>
2012-09-04 22:32:45 +0000

    Removed superfluous tests. Tweaked testing parameters.
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_inverted_index_updates_new@1879 123451ca-8445-de46-9d55-352943316053

--- hyracks-test-support/src/main/java/edu/uci/ics/hyracks/storage/am/config/AccessMethodTestsConfig.java
+++ hyracks-test-support/src/main/java/edu/uci/ics/hyracks/storage/am/config/AccessMethodTestsConfig.java
@@ -74,12 +74,12 @@ public class AccessMethodTestsConfig {
     public static final int LSM_INVINDEX_DISK_MAX_OPEN_FILES = 1000;
     public static final int LSM_INVINDEX_MEM_PAGE_SIZE = 1024;
     public static final int LSM_INVINDEX_MEM_NUM_PAGES = 100;
-    public static final int LSM_INVINDEX_HYRACKS_FRAME_SIZE = 256;
+    public static final int LSM_INVINDEX_HYRACKS_FRAME_SIZE = 32768;
     // Test parameters.
-    public static final int LSM_INVINDEX_NUM_DOCS_TO_INSERT = 100;
+    public static final int LSM_INVINDEX_NUM_DOCS_TO_INSERT = 10000;
     // Used for full-fledged search test.
-    public static final int LSM_INVINDEX_NUM_DOC_QUERIES = 1000;
-    public static final int LSM_INVINDEX_NUM_RANDOM_QUERIES = 1000;
+    public static final int LSM_INVINDEX_NUM_DOC_QUERIES = 500;
+    public static final int LSM_INVINDEX_NUM_RANDOM_QUERIES = 500;
     // Used for non-search tests to sanity check index searches.
     public static final int LSM_INVINDEX_TINY_NUM_DOC_QUERIES = 200;
     public static final int LSM_INVINDEX_TINY_NUM_RANDOM_QUERIES = 200;
@@ -89,7 +89,7 @@ public class AccessMethodTestsConfig {
     public static final int LSM_INVINDEX_NUM_DELETE_ROUNDS = 3;
     // Allocate a generous size to make sure we have enough elements for all tests.
     public static final int LSM_INVINDEX_SCAN_COUNT_ARRAY_SIZE = 1000000;
-    public static final int LSM_INVINDEX_MULTITHREAD_NUM_OPERATIONS = 10000;
+    public static final int LSM_INVINDEX_MULTITHREAD_NUM_OPERATIONS = 5000;
     
 }
 
