a8b74c8d98ff7640eae229887bb2d1b0d579f95f
kisskys <kisskys@123451ca-8445-de46-9d55-352943316053>
2012-10-05 20:08:44 +0000

    added NOOP indexOperation
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_lsm_tree@1955 123451ca-8445-de46-9d55-352943316053

--- hyracks-storage-am-common/src/main/java/edu/uci/ics/hyracks/storage/am/common/ophelpers/IndexOperation.java
+++ hyracks-storage-am-common/src/main/java/edu/uci/ics/hyracks/storage/am/common/ophelpers/IndexOperation.java
@@ -16,5 +16,5 @@
 package edu.uci.ics.hyracks.storage.am.common.ophelpers;
 
 public enum IndexOperation {
-	INSERT, DELETE, UPDATE, UPSERT, SEARCH, DISKORDERSCAN, PHYSICALDELETE
+	INSERT, DELETE, UPDATE, UPSERT, SEARCH, DISKORDERSCAN, PHYSICALDELETE, NOOP
 }
