59f2be6e6b4573b3e7ab71da173efccf75a2e670
buyingyi <buyingyi@123451ca-8445-de46-9d55-352943316053>
2012-10-09 21:07:20 +0000

    update startnc script and the driver
    git-svn-id: https://hyracks.googlecode.com/svn/branches/fullstack_staging@1977 123451ca-8445-de46-9d55-352943316053

--- pregelix/pregelix-core/src/main/resources/scripts/startnc.sh
+++ pregelix/pregelix-core/src/main/resources/scripts/startnc.sh
@@ -50,5 +50,4 @@ export JAVA_OPTS=$NCJAVA_OPTS
 cd $NCTMP_DIR
 
 #Launch hyracks nc
-chmod -R 755 $HYRACKS_HOME
 $HYRACKS_HOME/hyracks-server/target/appassembler/bin/hyracksnc -cc-host $CCHOST -cc-port $CC_CLUSTERPORT -cluster-net-ip-address $IPADDR  -data-ip-address $IPADDR -node-id $NODEID -iodevices "${IO_DIRS}" &> $NCLOGS_DIR/$NODEID.log &
