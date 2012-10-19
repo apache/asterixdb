932aa6fea04f865dfc815d170277f5129f0ea0df
buyingyi <buyingyi@123451ca-8445-de46-9d55-352943316053>
2012-10-19 03:35:58 +0000

    fix the startnc script for IPv6
    git-svn-id: https://hyracks.googlecode.com/svn/branches/fullstack_staging@2137 123451ca-8445-de46-9d55-352943316053

--- pregelix/pregelix-core/src/main/resources/scripts/startnc.sh
+++ pregelix/pregelix-core/src/main/resources/scripts/startnc.sh
@@ -37,7 +37,7 @@ then
 	#Get IP Address
 	IPADDR=`/sbin/ifconfig eth0 | grep "inet addr" | awk '{print $2}' | cut -f 2 -d ':'`
 else
-	IPADDR=`/sbin/ifconfig en1 | grep "inet" | awk '{print $2}' | cut -f 2 -d ':'`
+	IPADDR=`/sbin/ifconfig en1 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
 fi
 
 #Get node ID
