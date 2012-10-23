f0039cb8e00322d78ce6735a6d7eae5c6a865098
buyingyi <buyingyi@123451ca-8445-de46-9d55-352943316053>
2012-10-23 05:14:18 +0000

    update scripts
    git-svn-id: https://hyracks.googlecode.com/svn/branches/fullstack_staging@2243 123451ca-8445-de46-9d55-352943316053

--- pregelix/pregelix-core/src/main/resources/scripts/startnc.sh
+++ pregelix/pregelix-core/src/main/resources/scripts/startnc.sh
@@ -4,7 +4,7 @@ MY_NAME=`hostname`
 #Get the IP address of the cc
 CCHOST_NAME=`cat conf/master`
 CURRENT_PATH=`pwd`
-CCHOST=`bin/getip.sh`
+CCHOST=`ssh ${CCHOST_NAME} "cd ${CURRENT_PATH}; bin/getip.sh"`
 
 #Import cluster properties
 . conf/cluster.properties
