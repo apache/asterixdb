79268c8c5e27f8b1954898294e87927bbb4f71a3
buyingyi <buyingyi@123451ca-8445-de46-9d55-352943316053>
2012-10-26 18:18:07 +0000

    fix the language problem in the getip script
    git-svn-id: https://hyracks.googlecode.com/svn/branches/fullstack_staging@2358 123451ca-8445-de46-9d55-352943316053

--- pregelix/pregelix-core/src/main/resources/scripts/getip.sh
+++ pregelix/pregelix-core/src/main/resources/scripts/getip.sh
@@ -5,8 +5,17 @@ LINUX_OS='Linux'
 if [ $OS_NAME = $LINUX_OS ];
 then
         #Get IP Address
-        IPADDR=`/sbin/ifconfig eth0 | grep "inet addr" | awk '{print $2}' | cut -f 2 -d ':'`
+        IPADDR=`/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
+	if [ "$IPADDR" = "" ]
+        then
+		IPADDR=`/sbin/ifconfig lo | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
+        fi 
 else
         IPADDR=`/sbin/ifconfig en1 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
+	if [ "$IPADDR" = "" ]
+        then
+                IPADDR=`/sbin/ifconfig lo0 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
+        fi
+
 fi
 echo $IPADDR
