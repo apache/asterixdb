c11728dc26198c79ae1218346d87f7158b5e0c74
madhusudancs <madhusudancs@123451ca-8445-de46-9d55-352943316053>
2012-11-29 09:27:22 +0000

    Fix a formatting issue with the comment.
    git-svn-id: https://hyracks.googlecode.com/svn/branches/fullstack_hyracks_result_distribution@2536 123451ca-8445-de46-9d55-352943316053

--- hyracks/hyracks-control/hyracks-control-cc/src/main/java/edu/uci/ics/hyracks/control/cc/dataset/DatasetDirectoryService.java
+++ hyracks/hyracks-control/hyracks-control-cc/src/main/java/edu/uci/ics/hyracks/control/cc/dataset/DatasetDirectoryService.java
@@ -19,16 +19,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import edu.uci.ics.hyracks.api.comm.NetworkAddress;
+import edu.uci.ics.hyracks.api.dataset.DatasetDirectoryRecord;
 import edu.uci.ics.hyracks.api.dataset.IDatasetDirectoryService;
 import edu.uci.ics.hyracks.api.dataset.ResultSetId;
 import edu.uci.ics.hyracks.api.exceptions.HyracksDataException;
 import edu.uci.ics.hyracks.api.job.JobId;
 
 /**
- * TODO: The potential perils of this global dataset directory service implementation is that, the jobs location information
- * is never evicted from the memory and the memory usage grows as the number of jobs in the system grows. What we should
- * possibly do is, add an API call for the client to say that it received everything it has to for the job (after it receives
- * all the results) completely. Then we can just get rid of the location information for that job.
+ * TODO(madhusudancs): The potential perils of this global dataset directory service implementation is that, the jobs
+ * location information is never evicted from the memory and the memory usage grows as the number of jobs in the system
+ * grows. What we should possibly do is, add an API call for the client to say that it received everything it has to for
+ * the job (after it receives all the results) completely. Then we can just get rid of the location information for that
+ * job.
  */
 public class DatasetDirectoryService implements IDatasetDirectoryService {
     private final Map<JobId, Map<ResultSetId, DatasetDirectoryRecord[]>> jobResultLocationsMap;
