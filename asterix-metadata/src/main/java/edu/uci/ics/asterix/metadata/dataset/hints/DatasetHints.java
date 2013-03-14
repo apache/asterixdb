be91f16f2247cd44d7ed6c4fb20ed335e6bd4d6b
salsubaiee <salsubaiee@eaa15691-b419-025a-1212-ee371bd00084>
2013-03-14 22:53:08 +0000

    Propagate the cardinality hint properly to the lsm-indexes to be used by the bulkload to create the corresponding bloom filter.
    git-svn-id: https://asterixdb.googlecode.com/svn/branches/asterix_lsm_stabilization@1328 eaa15691-b419-025a-1212-ee371bd00084

--- asterix-metadata/src/main/java/edu/uci/ics/asterix/metadata/dataset/hints/DatasetHints.java
+++ asterix-metadata/src/main/java/edu/uci/ics/asterix/metadata/dataset/hints/DatasetHints.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.uci.ics.asterix.translator;
+package edu.uci.ics.asterix.metadata.dataset.hints;
 
 import java.util.HashSet;
 import java.util.Set;
--- asterix-metadata/src/main/java/edu/uci/ics/asterix/metadata/dataset/hints/IHint.java
+++ asterix-metadata/src/main/java/edu/uci/ics/asterix/metadata/dataset/hints/IHint.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.uci.ics.asterix.translator;
+package edu.uci.ics.asterix.metadata.dataset.hints;
 
 import edu.uci.ics.hyracks.algebricks.common.utils.Pair;
 
