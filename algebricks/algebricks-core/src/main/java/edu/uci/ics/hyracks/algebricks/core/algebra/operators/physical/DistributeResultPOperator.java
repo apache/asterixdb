9d24d6ef67f6c6db5f31197b88836ae661e4816e
Madhusudan.C.S <madhusudancs@gmail.com>
2013-04-17 14:30:09 -0700

    Do not provide partition constraints for result distribution operator, it must run on all the partitions where the previous operator ran.

--- algebricks/algebricks-core/src/main/java/edu/uci/ics/hyracks/algebricks/core/algebra/operators/physical/DistributeResultPOperator.java
+++ algebricks/algebricks-core/src/main/java/edu/uci/ics/hyracks/algebricks/core/algebra/operators/physical/DistributeResultPOperator.java
@@ -103,7 +103,6 @@ public class DistributeResultPOperator extends AbstractPhysicalOperator {
                 resultOp.getDataSink(), columns, pf, inputDesc, false, spec);
 
         builder.contributeHyracksOperator(resultOp, runtimeAndConstraints.first);
-        builder.contributeAlgebricksPartitionConstraint(runtimeAndConstraints.first, runtimeAndConstraints.second);
         ILogicalOperator src = resultOp.getInputs().get(0).getValue();
         builder.contributeGraphEdge(src, 0, resultOp, 0);
     }
