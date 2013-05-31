dc4046843e7a5d3c8f7efb1285020f6ed37e5c3c
pouria.pirzadeh <pouria.pirzadeh@gmail.com>
2013-05-30 17:29:58 -0700

    Addressing Yingyi's Review

--- hyracks/hyracks-api/src/main/java/edu/uci/ics/hyracks/api/dataflow/value/IPredicateEvaluatorFactory.java
+++ hyracks/hyracks-api/src/main/java/edu/uci/ics/hyracks/api/dataflow/value/IPredicateEvaluatorFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2010 by The Regents of the University of California
+ * Copyright 2009-2013 by The Regents of the University of California
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * you may obtain a copy of the License from
@@ -16,6 +16,10 @@
 package edu.uci.ics.hyracks.api.dataflow.value;
 
 import java.io.Serializable;
+
+/*
+ * Provides PredicateEvaluator for equi-join related operators 
+ */
 
 public interface IPredicateEvaluatorFactory extends Serializable {
 	public IPredicateEvaluator createPredicateEvaluator();
