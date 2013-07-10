c04ac3c0e15362725d919b01242cc2714054aa7b
ramangrover29 <ramangrover29@gmail.com>
2013-07-10 08:34:48 -0700

    added FeedUtil class

---
+++ asterix-metadata/src/main/java/edu/uci/ics/asterix/metadata/feeds/FeedUtil.java
@@ -0,0 +1,12 @@
+package edu.uci.ics.asterix.metadata.feeds;
+
+import edu.uci.ics.asterix.metadata.entities.FeedActivity;
+import edu.uci.ics.asterix.metadata.entities.FeedActivity.FeedActivityType;
+
+public class FeedUtil {
+
+    public static boolean isFeedActive(FeedActivity feedActivity) {
+        return (feedActivity != null && (!feedActivity.getActivityType().equals(FeedActivityType.FEED_END) && !feedActivity
+                .getActivityType().equals(FeedActivityType.FEED_FAILURE)));
+    }
+}
