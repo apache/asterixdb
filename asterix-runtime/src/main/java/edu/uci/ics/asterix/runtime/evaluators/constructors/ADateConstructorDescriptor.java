ee7e1b2428abfe0d496cd63b9c81d7eb0c990ec5
JArod <jarodwen@gmail.com>
2013-08-14 03:08:05 -0700

    Addressed Till's comments on incorrect comment.

--- asterix-runtime/src/main/java/edu/uci/ics/asterix/runtime/evaluators/constructors/ADateConstructorDescriptor.java
+++ asterix-runtime/src/main/java/edu/uci/ics/asterix/runtime/evaluators/constructors/ADateConstructorDescriptor.java
@@ -83,7 +83,7 @@ public class ADateConstructorDescriptor extends AbstractScalarFunctionDynamicDes
 
                                 int stringLength = (serString[1] & 0xff << 8) + (serString[2] & 0xff << 0);
 
-                                // the string to be parsed should be at least 14 characters: YYYYMMDD
+                                // the string to be parsed should be at least 8 characters: YYYYMMDD
                                 if (stringLength < 8) {
                                     throw new AlgebricksException(errorMessage
                                             + ": the string length should be at least 8 (YYYYMMDD) but it is "
