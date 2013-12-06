91e0b23f79c9b5b2606a00ec76dd4ff90fa3b25a
zheilbron <zheilbron@gmail.com>
2013-12-06 15:37:55 -0800

    use the typeheirarchy for type checking in the admdataparser

--- asterix-runtime/src/main/java/edu/uci/ics/asterix/runtime/operators/file/ADMDataParser.java
+++ asterix-runtime/src/main/java/edu/uci/ics/asterix/runtime/operators/file/ADMDataParser.java
@@ -22,8 +22,6 @@ import java.util.BitSet;
 import java.util.List;
 import java.util.Queue;
 
-import edu.uci.ics.asterix.runtime.operators.file.adm.AdmLexer;
-import edu.uci.ics.asterix.runtime.operators.file.adm.AdmLexerException;
 import edu.uci.ics.asterix.builders.IARecordBuilder;
 import edu.uci.ics.asterix.builders.IAsterixListBuilder;
 import edu.uci.ics.asterix.builders.OrderedListBuilder;
@@ -51,7 +49,10 @@ import edu.uci.ics.asterix.om.types.ATypeTag;
 import edu.uci.ics.asterix.om.types.AUnionType;
 import edu.uci.ics.asterix.om.types.AUnorderedListType;
 import edu.uci.ics.asterix.om.types.IAType;
+import edu.uci.ics.asterix.om.types.hierachy.ATypeHierarchy;
 import edu.uci.ics.asterix.om.util.NonTaggedFormatUtil;
+import edu.uci.ics.asterix.runtime.operators.file.adm.AdmLexer;
+import edu.uci.ics.asterix.runtime.operators.file.adm.AdmLexerException;
 import edu.uci.ics.hyracks.api.exceptions.HyracksDataException;
 import edu.uci.ics.hyracks.data.std.util.ArrayBackedValueStorage;
 
@@ -416,13 +417,14 @@ public class ADMDataParser extends AbstractDataParser implements IDataParser {
             return true;
 
         if (aObjectType.getTypeTag() != ATypeTag.UNION) {
-            if (expectedTypeTag == aObjectType.getTypeTag())
-                return true;
+            return ATypeHierarchy.canPromote(expectedTypeTag, aObjectType.getTypeTag());
         } else { // union
             unionList = ((AUnionType) aObjectType).getUnionList();
-            for (int i = 0; i < unionList.size(); i++)
-                if (unionList.get(i).getTypeTag() == expectedTypeTag)
+            for (IAType t : unionList) {
+                if (ATypeHierarchy.canPromote(t.getTypeTag(), expectedTypeTag)) {
                     return true;
+                }
+            }
         }
         return false;
     }
@@ -847,7 +849,7 @@ public class ADMDataParser extends AbstractDataParser implements IDataParser {
         } catch (Exception e) {
             throw new AsterixException(e);
         }
-        throw new AsterixException(mismatchErrorMessage + objectType.getTypeName());
+        throw new AsterixException(mismatchErrorMessage + objectType.getTypeName() + ". Got " + typeTag + " instead.");
     }
 
     private void parseBoolean(String bool, DataOutput out) throws AsterixException {
