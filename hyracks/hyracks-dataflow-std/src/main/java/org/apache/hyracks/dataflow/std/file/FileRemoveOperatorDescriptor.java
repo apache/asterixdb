0661036306c80ee45ed3f60f91ea38af46a64cff
Abdullah Alamoudi <bamousaa@gmail.com>
2016-04-04 09:45:29 +0300

    Enable Remove Quietly for Remove Directory Jobs
    Change-Id: I00a60bf4ac6d64295224c2cebc28fb22f92e77fd
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/775
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Reviewed-by: Murtadha Hubail <hubailmor@gmail.com>

--- hyracks/hyracks-dataflow-std/src/main/java/org/apache/hyracks/dataflow/std/file/FileRemoveOperatorDescriptor.java
+++ hyracks/hyracks-dataflow-std/src/main/java/org/apache/hyracks/dataflow/std/file/FileRemoveOperatorDescriptor.java
@@ -37,10 +37,13 @@ import org.apache.hyracks.dataflow.std.base.AbstractSingleActivityOperatorDescri
 public class FileRemoveOperatorDescriptor extends AbstractSingleActivityOperatorDescriptor {
 
     private final IFileSplitProvider fileSplitProvider;
+    private final boolean quietly;
 
-    public FileRemoveOperatorDescriptor(IOperatorDescriptorRegistry spec, IFileSplitProvider fileSplitProvder) {
+    public FileRemoveOperatorDescriptor(IOperatorDescriptorRegistry spec, IFileSplitProvider fileSplitProvder,
+            boolean quietly) {
         super(spec, 0, 0);
         this.fileSplitProvider = fileSplitProvder;
+        this.quietly = quietly;
     }
 
     private static final long serialVersionUID = 1L;
@@ -62,10 +65,14 @@ public class FileRemoveOperatorDescriptor extends AbstractSingleActivityOperator
             @Override
             public void initialize() throws HyracksDataException {
                 File f = ioManager.getAbsoluteFileRef(deviceId, path).getFile();
-                try {
-                    FileUtils.deleteDirectory(f);
-                } catch (IOException e) {
-                    throw new HyracksDataException(e);
+                if (quietly) {
+                    FileUtils.deleteQuietly(f);
+                } else {
+                    try {
+                        FileUtils.deleteDirectory(f);
+                    } catch (IOException e) {
+                        throw new HyracksDataException(e);
+                    }
                 }
             }
 
