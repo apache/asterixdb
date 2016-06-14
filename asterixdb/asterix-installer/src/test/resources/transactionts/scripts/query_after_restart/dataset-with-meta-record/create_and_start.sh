11a0d306b7be86693708cb251b5af6a4289d5391
Michael Blow <mblow@apache.org>
2016-06-14 01:09:47 -0400

    ASTERIXDB-1475: Fix Integration Tests
    Ensure test feed classes are available on classpath for
    dataset-with-meta-record test.
    
    Change-Id: I1881becff30004a52711daaa475b177619010e47
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/922
    Reviewed-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Reviewed-by: abdullah alamoudi <bamousaa@gmail.com>

--- asterixdb/asterix-installer/src/test/resources/transactionts/scripts/query_after_restart/dataset-with-meta-record/create_and_start.sh
+++ asterixdb/asterix-installer/src/test/resources/transactionts/scripts/query_after_restart/dataset-with-meta-record/create_and_start.sh
@@ -1,1 +1,5 @@
 $MANAGIX_HOME/bin/managix create -n nc1 -c $MANAGIX_HOME/clusters/local/local.xml;
+$MANAGIX_HOME/bin/managix stop -n nc1;
+cp $MANAGIX_HOME/../../../asterix-external-data/target/asterix-external-data-*-tests.jar \
+    $MANAGIX_HOME/clusters/local/working_dir/asterix/repo/
+$MANAGIX_HOME/bin/managix start -n nc1;
