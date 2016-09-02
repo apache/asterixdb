8ed625629ead97b2fea470d40dc4447c3d53c5f6
Michael Blow <mblow@apache.org>
2016-09-02 13:23:09 -0400

    Start/Stop Sample Scripts for Windows
    - Fix windows binaries (ncservice, cc, nc, helper)
    - Add windows start/stop sample cluster scripts
    
    Change-Id: Id6eee2156a5c6e40e0e864c2b8be2c08593e71b1
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/1142
    Sonar-Qube: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Reviewed-by: Till Westmann <tillw@apache.org>
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>

---
+++ asterixdb/asterix-server/src/main/samples/local/bin/start-sample-cluster.bat
@@ -0,0 +1,83 @@
+@REM ------------------------------------------------------------
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements.  See the NOTICE file
+@REM distributed with this work for additional information
+@REM regarding copyright ownership.  The ASF licenses this file
+@REM to you under the Apache License, Version 2.0 (the
+@REM "License"); you may not use this file except in compliance
+@REM with the License.  You may obtain a copy of the License at
+@REM
+@REM   http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing,
+@REM software distributed under the License is distributed on an
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM KIND, either express or implied.  See the License for the
+@REM specific language governing permissions and limitations
+@REM under the License.
+@REM ------------------------------------------------------------
+
+@echo off
+setlocal
+if NOT DEFINED JAVA_HOME (
+  echo ERROR: JAVA_HOME not defined
+  goto :ERROR
+)
+REM ensure JAVA_HOME has no spaces nor quotes, since appassembler can't handle them
+for %%I in (%JAVA_HOME%) do (
+  set JAVA_HOME=%%~sI
+)
+
+set JAVACMD=%JAVA_HOME%\bin\java
+
+REM TODO(mblow): check java version, spaces in CWD
+
+set DIRNAME=%~dp0
+
+pushd %DIRNAME%\..
+set CLUSTERDIR=%cd%
+cd %CLUSTERDIR%\..\..
+set INSTALLDIR=%cd%
+set LOGSDIR=%CLUSTERDIR%\logs
+
+echo CLUSTERDIR=%CLUSTERDIR%
+echo INSTALLDIR=%INSTALLDIR%
+echo.
+cd %CLUSTERDIR%
+if NOT EXIST %LOGSDIR% (
+  mkdir %LOGSDIR%
+)
+call %INSTALLDIR%\bin\${HELPER_COMMAND} get_cluster_state -quiet
+
+IF %ERRORLEVEL% EQU 0 (
+  echo ERROR: sample cluster address [localhost:${LISTEN_PORT}] already in use
+  goto :ERROR
+)
+
+REM TODO(mblow): append timestamp to logs when starting
+
+echo Starting sample cluster...
+
+start /MIN "blue-nc" cmd /c "echo See output in %LOGSDIR%\blue-service.log && %INSTALLDIR%\bin\${NC_SERVICE_COMMAND} -logdir - -config-file %CLUSTERDIR%\conf\blue.conf >> %LOGSDIR%\blue-service.log 2>&1"
+start /MIN "red-nc" cmd /c "echo See output in %LOGSDIR%\red-service.log && %INSTALLDIR%\bin\${NC_SERVICE_COMMAND} -logdir - >> %LOGSDIR%\red-service.log 2>&1"
+start /MIN "cc" cmd /c "echo See output in %LOGSDIR%\cc.log && %INSTALLDIR%\bin\${CC_COMMAND} -config-file %CLUSTERDIR%\conf\cc.conf >>%LOGSDIR%\cc.log 2>&1"
+
+echo.
+echo Waiting for sample cluster [localhost:${LISTEN_PORT}] to be ready...
+call %INSTALLDIR%\bin\${HELPER_COMMAND} wait_for_cluster -quiet -timeout 30
+if %ERRORLEVEL% NEQ 0 (
+  echo ERROR: cluster did not start successfully
+  echo See output in %LOGSDIR%\
+  goto :ERROR
+)
+echo Sample cluster [localhost:${LISTEN_PORT}] is ready...
+echo.
+echo See output in %LOGSDIR%
+echo.
+popd
+endlocal
+
+:ERROR
+popd
+endlocal
+exit /B 1
