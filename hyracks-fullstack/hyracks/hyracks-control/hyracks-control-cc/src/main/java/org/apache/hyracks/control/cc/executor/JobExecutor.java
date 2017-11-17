7a117402258433ac95b9c8e9d8ffb1ba036bb26c
Xikui Wang <xkkwww@gmail.com>
2017-11-17 10:28:33 -0800

    [ASTERIXDB-2164][HYR] Fix node assignment bug for unbounded task
    - user model changes: no
    - storage format changes: no
    - interface changes: no
    
    Details:
    The upper bound of node selection is mistakenly set to 1. Change that to
    the size of live nodes can fix the issue.
    Change-Id: I06e1b38bf74038d975a6dd17052e46e74fe1fc02
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/2148
    Sonar-Qube: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Integration-Tests: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Contrib: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Reviewed-by: Till Westmann <tillw@apache.org>

--- hyracks-fullstack/hyracks/hyracks-control/hyracks-control-cc/src/main/java/org/apache/hyracks/control/cc/executor/JobExecutor.java
+++ hyracks-fullstack/hyracks/hyracks-control/hyracks-control-cc/src/main/java/org/apache/hyracks/control/cc/executor/JobExecutor.java
@@ -447,7 +447,7 @@ public class JobExecutor {
             Object location = solver.getValue(pLocationExpr);
             if (location == null) {
                 // pick any
-                nodeId = liveNodes.toArray(new String[liveNodes.size()])[random.nextInt(1) % liveNodes.size()];
+                nodeId = liveNodes.toArray(new String[liveNodes.size()])[random.nextInt(liveNodes.size())];
             } else if (location instanceof String) {
                 nodeId = (String) location;
             } else if (location instanceof String[]) {
