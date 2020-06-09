8ef98896877ee33d0480a916ed070487634f29d7
Michael Blow <michael.blow@couchbase.com>
2020-06-09 16:48:11 -0400

    [NO ISSUE][MISC] HyracksException.getParams() type, += Throwing helpers
    Change-Id: I0aa740452d40719da33bcb25ecec6c09e45c9dd1
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/c/asterixdb/+/6703
    Integration-Tests: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Reviewed-by: Ali Alsuliman <ali.al.solaiman@gmail.com>

--- asterixdb/asterix-app/src/test/java/org/apache/asterix/test/runtime/ExecutionTestUtil.java
+++ asterixdb/asterix-app/src/test/java/org/apache/asterix/test/runtime/ExecutionTestUtil.java
@@ -19,7 +19,6 @@
 package org.apache.asterix.test.runtime;
 
 import java.io.File;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +33,7 @@ import org.apache.asterix.testframework.xml.TestSuite;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.hyracks.api.config.IOption;
 import org.apache.hyracks.control.nc.NodeControllerService;
+import org.apache.hyracks.util.ThrowingSupplier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -123,8 +123,12 @@ public class ExecutionTestUtil {
             HDFSCluster.getInstance().cleanup();
         }
 
+        saveFailedXml(() -> File.createTempFile("failed", ".xml"));
+    }
+
+    public static void saveFailedXml(ThrowingSupplier<File> destFileSupplier) throws Exception {
         if (FailedGroup != null && FailedGroup.getTestCase().size() > 0) {
-            File temp = File.createTempFile("failed", ".xml");
+            File failedFile = destFileSupplier.get();
             javax.xml.bind.JAXBContext jaxbCtx = null;
             jaxbCtx = javax.xml.bind.JAXBContext.newInstance(TestSuite.class.getPackage().getName());
             javax.xml.bind.Marshaller marshaller = null;
@@ -133,11 +137,12 @@ public class ExecutionTestUtil {
             TestSuite failedSuite = new TestSuite();
             failedSuite.setResultOffsetPath("results");
             failedSuite.setQueryOffsetPath("queries");
+            failedSuite.setQueryFileExtension(".sqlpp");
             failedSuite.getTestGroup().add(FailedGroup);
-            marshaller.marshal(failedSuite, temp);
-            System.err.println("The failed.xml is written to :" + temp.getAbsolutePath()
-                    + ". You can copy it to only.xml by the following cmd:" + "\rcp " + temp.getAbsolutePath() + " "
-                    + Paths.get("./src/test/resources/runtimets/only.xml").toAbsolutePath());
+            marshaller.marshal(failedSuite, failedFile);
+            LOGGER.error("#####");
+            LOGGER.error("##### the failed.xml is written to {}", failedFile.getAbsolutePath());
+            LOGGER.error("#####");
         }
     }
 
