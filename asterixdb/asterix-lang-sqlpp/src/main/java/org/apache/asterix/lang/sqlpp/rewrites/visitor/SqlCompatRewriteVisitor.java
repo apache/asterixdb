0b6f85cc11add52a457427e9a5a94f3bdb632fc5
Dmitry Lychagin <dmitry.lychagin@couchbase.com>
2021-10-18 15:08:37 -0700

    [NO ISSUE][COMP] Support IN scalar in SQL-compat mode
    - user model changes: no
    - storage format changes: no
    - interface changes: no
    
    Details:
    - Support NOT? IN scalar when evaluated in SQL-compatible mode
    - Add tests
    
    Change-Id: I660ac9d6cbdf1ea6dcf25fd2b046aa75c5e7c3e9
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/c/asterixdb/+/13743
    Integration-Tests: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Reviewed-by: Dmitry Lychagin <dmitry.lychagin@couchbase.com>
    Reviewed-by: Ali Alsuliman <ali.al.solaiman@gmail.com>

--- asterixdb/asterix-lang-sqlpp/src/main/java/org/apache/asterix/lang/sqlpp/rewrites/visitor/SqlCompatRewriteVisitor.java
+++ asterixdb/asterix-lang-sqlpp/src/main/java/org/apache/asterix/lang/sqlpp/rewrites/visitor/SqlCompatRewriteVisitor.java
@@ -19,16 +19,27 @@
 
 package org.apache.asterix.lang.sqlpp.rewrites.visitor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.asterix.common.exceptions.CompilationException;
+import org.apache.asterix.common.functions.FunctionSignature;
 import org.apache.asterix.lang.common.base.Expression;
 import org.apache.asterix.lang.common.base.ILangExpression;
+import org.apache.asterix.lang.common.expression.CallExpr;
+import org.apache.asterix.lang.common.expression.OperatorExpr;
+import org.apache.asterix.lang.common.struct.OperatorType;
 import org.apache.asterix.lang.sqlpp.clause.Projection;
 import org.apache.asterix.lang.sqlpp.visitor.base.AbstractSqlppSimpleExpressionVisitor;
+import org.apache.asterix.om.functions.BuiltinFunctions;
+import org.apache.hyracks.algebricks.core.algebra.functions.FunctionIdentifier;
+import org.apache.hyracks.api.exceptions.SourceLocation;
 
 /**
  * Applies initial rewritings for "SQL-compatible" evaluation mode
  * <ol>
- * <li>Rewrites "SELECT *" into "SELECT *.*".
+ * <li>Rewrites {@code SELECT *} into {@code SELECT *.*}
+ * <li>Rewrites {@code NOT? IN expr} into {@code NOT? IN to_array(expr)} if {@code expr} can return a non-list
  * </ol>
  */
 public final class SqlCompatRewriteVisitor extends AbstractSqlppSimpleExpressionVisitor {
@@ -39,5 +50,45 @@ public final class SqlCompatRewriteVisitor extends AbstractSqlppSimpleExpression
             projection.setKind(Projection.Kind.EVERY_VAR_STAR);
         }
         return super.visit(projection, arg);
+    }
+
+    @Override
+    public Expression visit(OperatorExpr opExpr, ILangExpression arg) throws CompilationException {
+        List<OperatorType> opTypeList = opExpr.getOpList();
+        if (opTypeList.size() == 1) {
+            switch (opTypeList.get(0)) {
+                case IN:
+                case NOT_IN:
+                    List<Expression> exprList = opExpr.getExprList();
+                    Expression arg1 = exprList.get(1);
+                    if (!alwaysReturnsList(arg1)) {
+                        List<Expression> newExprList = new ArrayList<>(2);
+                        newExprList.add(exprList.get(0));
+                        newExprList.add(createCallExpr(BuiltinFunctions.TO_ARRAY, arg1, opExpr.getSourceLocation()));
+                        opExpr.setExprList(newExprList);
+                    }
+                    break;
+            }
+        }
+        return super.visit(opExpr, arg);
+    }
+
+    private static boolean alwaysReturnsList(Expression expr) {
+        switch (expr.getKind()) {
+            case LIST_CONSTRUCTOR_EXPRESSION:
+            case LIST_SLICE_EXPRESSION:
+            case SELECT_EXPRESSION:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private static CallExpr createCallExpr(FunctionIdentifier fid, Expression inExpr, SourceLocation sourceLoc) {
+        List<Expression> argList = new ArrayList<>(1);
+        argList.add(inExpr);
+        CallExpr callExpr = new CallExpr(new FunctionSignature(fid), argList);
+        callExpr.setSourceLocation(sourceLoc);
+        return callExpr;
     }
 }
