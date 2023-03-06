db2d76a27c9531ba60c97caa30ee634be2218eee
Vijay Sarathy <vijay.sarathy@couchbase.com>
2023-03-06 15:49:28 -0800

    [ASTERIXDB-3125][COMP] Select operator has zero cost and cardinality
    [ASTERIXDB-3126][COMP] CBO: hint productivity NPE if missing cardinality
    estimate on collection
    
    - user model changes: no
    - storage format changes: no
    - interface changes: no
    
    
    Change-Id: I7cbff25b14346ca8eca5baa7e0fe8fb1da72f270
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/c/asterixdb/+/17408
    Reviewed-by: Ali Alsuliman <ali.al.solaiman@gmail.com>
    Tested-by: Ali Alsuliman <ali.al.solaiman@gmail.com>
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Integration-Tests: Ali Alsuliman <ali.al.solaiman@gmail.com>

--- asterixdb/asterix-algebra/src/main/java/org/apache/asterix/optimizer/rules/cbo/EnumerateJoinsRule.java
+++ asterixdb/asterix-algebra/src/main/java/org/apache/asterix/optimizer/rules/cbo/EnumerateJoinsRule.java
@@ -434,8 +434,9 @@ public class EnumerateJoinsRule implements IAlgebraicRewriteRule {
             HashMap<EmptyTupleSourceOperator, ILogicalOperator> joinLeafInputsHashMap) {
         ILogicalOperator leftInput = joinLeafInputsHashMap.get(plan.getEmptyTupleSourceOp());
         skipAllIndexes(plan, leftInput);
-        if (leftInput.getOperatorTag() == LogicalOperatorTag.SELECT) {
-            addCardCostAnnotations(leftInput, plan);
+        ILogicalOperator selOp = findSelectOrDataScan(leftInput);
+        if (selOp != null) {
+            addCardCostAnnotations(selOp, plan);
         }
         addCardCostAnnotations(findDataSourceScanOperator(leftInput), plan);
     }
@@ -493,8 +494,9 @@ public class EnumerateJoinsRule implements IAlgebraicRewriteRule {
             // leaf
             ILogicalOperator leftInput = joinLeafInputsHashMap.get(leftPlan.getEmptyTupleSourceOp());
             skipAllIndexes(leftPlan, leftInput);
-            if (leftInput.getOperatorTag() == LogicalOperatorTag.SELECT) {
-                addCardCostAnnotations(leftInput, leftPlan);
+            ILogicalOperator selOp = findSelectOrDataScan(leftInput);
+            if (selOp != null) {
+                addCardCostAnnotations(selOp, leftPlan);
             }
             joinOp.getInputs().get(0).setValue(leftInput);
             addCardCostAnnotations(findDataSourceScanOperator(leftInput), leftPlan);
@@ -510,8 +512,9 @@ public class EnumerateJoinsRule implements IAlgebraicRewriteRule {
             // leaf
             ILogicalOperator rightInput = joinLeafInputsHashMap.get(rightPlan.getEmptyTupleSourceOp());
             skipAllIndexes(rightPlan, rightInput);
-            if (rightInput.getOperatorTag() == LogicalOperatorTag.SELECT) {
-                addCardCostAnnotations(rightInput, rightPlan);
+            ILogicalOperator selOp = findSelectOrDataScan(rightInput);
+            if (selOp != null) {
+                addCardCostAnnotations(selOp, rightPlan);
             }
             joinOp.getInputs().get(1).setValue(rightInput);
             addCardCostAnnotations(findDataSourceScanOperator(rightInput), rightPlan);
