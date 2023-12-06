43b62b0c1f251bd9113c58572349e25c9b9da767
Peeyush Gupta <peeyush.gupta@couchbase.com>
2023-12-05 16:28:37 -0800

    [ASTERIXDB-3316][COMP] Stack overflow during query compilation
    - user model changes: no
    - storage format changes: no
    - interface changes: no
    
    Change-Id: I0011cd5633bbf4f57fcf81566170f6989fe2565d
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/c/asterixdb/+/18003
    Integration-Tests: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Reviewed-by: Peeyush Gupta <peeyush.gupta@couchbase.com>
    Reviewed-by: Ali Alsuliman <ali.al.solaiman@gmail.com>

--- asterixdb/asterix-algebra/src/main/java/org/apache/asterix/optimizer/rules/CancelUnnestWithNestedListifyRule.java
+++ asterixdb/asterix-algebra/src/main/java/org/apache/asterix/optimizer/rules/CancelUnnestWithNestedListifyRule.java
@@ -219,6 +219,10 @@ public class CancelUnnestWithNestedListifyRule implements IAlgebraicRewriteRule 
         if (arg0.getExpressionTag() != LogicalExpressionTag.VARIABLE) {
             return false;
         }
+        AbstractLogicalOperator aggChild = (AbstractLogicalOperator) agg.getInputs().get(0).getValue();
+        if (aggChild.getOperatorTag() == LogicalOperatorTag.AGGREGATE) {
+            return false;
+        }
         LogicalVariable paramVar = ((VariableReferenceExpression) arg0).getVariableReference();
 
         ArrayList<LogicalVariable> assgnVars = new ArrayList<LogicalVariable>(1);
@@ -246,6 +250,9 @@ public class CancelUnnestWithNestedListifyRule implements IAlgebraicRewriteRule 
             Mutable<ILogicalOperator> bottomOpRef = aggInputOpRef;
             AbstractLogicalOperator bottomOp = (AbstractLogicalOperator) bottomOpRef.getValue();
             while (bottomOp.getOperatorTag() != LogicalOperatorTag.NESTEDTUPLESOURCE) {
+                if (bottomOp.getOperatorTag() == LogicalOperatorTag.AGGREGATE) {
+                    return false;
+                }
                 bottomOpRef = bottomOp.getInputs().get(0);
                 bottomOp = (AbstractLogicalOperator) bottomOpRef.getValue();
             }
