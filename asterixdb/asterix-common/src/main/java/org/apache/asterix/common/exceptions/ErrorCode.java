17775942562acd492345bab49abecef5e1b0325a
Hussain Towaileb <Hussain.Towaileb@couchbase.com>
2024-03-19 18:42:14 +0300

    [NO ISSUE] Error code for invalide COPY TO key evaluation
    Change-Id: I655be98e2dfd95482747befe17f9151697eacb11
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/c/asterixdb/+/18205
    Integration-Tests: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Reviewed-by: Hussain Towaileb <hussainht@gmail.com>
    Reviewed-by: Wail Alkowaileet <wael.y.k@gmail.com>

--- asterixdb/asterix-common/src/main/java/org/apache/asterix/common/exceptions/ErrorCode.java
+++ asterixdb/asterix-common/src/main/java/org/apache/asterix/common/exceptions/ErrorCode.java
@@ -96,6 +96,7 @@ public enum ErrorCode implements IError {
     TYPE_MISMATCH_EXTRA_FIELD(66),
     UNSUPPORTED_COLUMN_TYPE(67),
     INVALID_KEY_TYPE(68),
+    FAILED_TO_READ_KEY(69),
 
     UNSUPPORTED_JRE(100),
 
