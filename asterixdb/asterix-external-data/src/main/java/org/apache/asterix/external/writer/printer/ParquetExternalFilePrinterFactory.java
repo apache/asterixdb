50d67194d47c8b9f63bf5c325405f22a4d19a02b
preetham0202 <preetham.poluparthi@couchbase.com>
2025-02-13 16:42:41 +0530

    [ASTERIXDB-3392] Minor formatting COPY TO parquet
    - user model changes: no
    - storage format changes: no
    - interface changes: no
    
    Ext-ref: MB-65167
    Change-Id: I009fc9e2d4e6cde0f4469b0e6979bf625eae7576
    Reviewed-on: https://asterix-gerrit.ics.uci.edu/c/asterixdb/+/19414
    Integration-Tests: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Tested-by: Jenkins <jenkins@fulliautomatix.ics.uci.edu>
    Reviewed-by: <preethampoluparthi@gmail.com>
    Reviewed-by: Hussain Towaileb <hussainht@gmail.com>

--- asterixdb/asterix-external-data/src/main/java/org/apache/asterix/external/writer/printer/ParquetExternalFilePrinterFactory.java
+++ asterixdb/asterix-external-data/src/main/java/org/apache/asterix/external/writer/printer/ParquetExternalFilePrinterFactory.java
@@ -30,9 +30,9 @@ import org.apache.parquet.schema.MessageType;
 
 public class ParquetExternalFilePrinterFactory implements IExternalPrinterFactory {
     private static final long serialVersionUID = 8971234908711235L;
-    // parquetInferSchema is for the case when the schema is inferred from the data, not provided by the user
+    // parquetInferredSchema is for the case when the schema is inferred from the data, not provided by the user
     // set During the runtime
-    private transient MessageType parquetInferSchema;
+    private transient MessageType parquetInferredSchema;
     // parquetProvidedSchema is for the case when the schema is provided by the user
     private ARecordType parquetProvidedSchema;
     private final IAType typeInfo;
@@ -61,14 +61,14 @@ public class ParquetExternalFilePrinterFactory implements IExternalPrinterFactor
         this.writerVersion = writerVersion;
     }
 
-    public void setParquetSchema(MessageType parquetInferSchema) {
-        this.parquetInferSchema = parquetInferSchema;
+    public void setParquetSchema(MessageType parquetInferredSchema) {
+        this.parquetInferredSchema = parquetInferredSchema;
     }
 
     @Override
     public IExternalPrinter createPrinter() {
-        if (parquetInferSchema != null) {
-            return new ParquetExternalFilePrinter(compressionCodecName, parquetInferSchema, typeInfo, rowGroupSize,
+        if (parquetInferredSchema != null) {
+            return new ParquetExternalFilePrinter(compressionCodecName, parquetInferredSchema, typeInfo, rowGroupSize,
                     pageSize, writerVersion);
         }
 
