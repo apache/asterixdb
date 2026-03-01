d6d7ced675373011278554346150485904f3a4f3
ongdisheng <ongdisheng@gmail.com>
2026-03-01 12:32:43 +0000

    [ASTERIXDB-2877][CSV] Fix multi-byte/emoji character corruption in CSV output

--- asterixdb/asterix-om/src/main/java/org/apache/asterix/dataflow/data/nontagged/printers/PrintTools.java
+++ asterixdb/asterix-om/src/main/java/org/apache/asterix/dataflow/data/nontagged/printers/PrintTools.java
@@ -312,7 +312,7 @@ public class PrintTools {
         boolean shouldQuote = forceQuote;
         if (!shouldQuote) {
             // Check if the string contains any special characters that require quoting
-            for (int i = position; i < maxPosition; i++) {
+            for (int i = position; i < maxPosition; i += UTF8StringUtil.charSize(b, i)) {
                 char c = UTF8StringUtil.charAt(b, i);
                 if (c == quote || c == '\r' || c == '\n' || c == escape || c == delimiter) {
                     shouldQuote = true;
@@ -340,9 +340,9 @@ public class PrintTools {
                 continue;
             }
 
-            // Write the character bytes
+            // Write the character bytes as raw UTF-8 bytes to avoid charset encoding bugs
             while (sz > 0) {
-                ps.print(c);
+                ps.write(b[position]);
                 ++position;
                 --sz;
             }
